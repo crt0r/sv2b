6c89a7e7ae97145397df2184ddd5c80420833f10
Timofey Chuchkanov <ttchuchkanov@yandex.ru>
2022-10-05 08:23:50 +0300

    change variables case to conventional

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ import { existsSync } from 'fs';
 
 $.verbose = false;
 
-const file_name = argv[2];
+const fileName = argv[2];
 
-if (!file_name || !existsSync(file_name)) {
-    console.log('File not found!');
+if (!fileName || !existsSync(fileName)) {
+    console.log(`File \`${ fileName ? fileName : '' }\` not found!`);
     exit(1);
 }
 
-const regex___var_to_replace = "\\s+#-.+$";
-const vars = (await $`grep -Po ${ regex___var_to_replace } ${ file_name }`)
+const regex___varToReplace = "\\s+#-.+$";
+const vars = (await $`grep -Po ${ regex___varToReplace } ${ fileName }`)
     .stdout
     .split('\n')
     .filter(e => e ? e : false)
     .map(e => e.trim());
     
-const vars_clean = vars.map(e => e.replace('#-', ''));
-const vars_base64 = (await convert_vars_to_base64(vars_clean)).map(e => e.replace('\n', ''));
-let file_content = fs.readFileSync(file_name).toString();
+const varsClean = vars.map(e => e.replace('#-', ''));
+const varsBase64 = (await convertVarsToBase64(varsClean)).map(e => e.replace('\n', ''));
+let fileContent = fs.readFileSync(fileName).toString();
 
 for (let i = 0; i < vars.length; i++) {
-    file_content = file_content.replace(vars[i], vars_base64[i]);
+    fileContent = fileContent.replace(vars[i], varsBase64[i]);
 }
 
-fs.writeFileSync(file_name, file_content);
+fs.writeFileSync(fileName, fileContent);
 
-async function convert_vars_to_base64(vars_clean) {
-    let vars_base64 = [];
+async function convertVarsToBase64(varsClean) {
+    let varsBase64 = [];
 
-    for (let i = 0; i < vars_clean.length; i++) {
-        const v = await $`echo ${ vars_clean[i] } | base64`;
-        vars_base64.push(v.stdout);
+    for (let i = 0; i < varsClean.length; i++) {
+        const v = await $`echo ${ varsClean[i] } | base64`;
+        varsBase64.push(v.stdout);
     }
 
-    return vars_base64;
+    return varsBase64;
 }
